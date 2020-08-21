29f7726dfe4df6081e28386f23353acbd6eca1d6
mahimasingh25 <mahimasingh2509@gmail.com>
2020-08-21 16:35:35 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
 function checklove() {
   var x = document.getElementById("yourname").value;
   var y = document.getElementById("crushname").value;
+  if (x == "") {
+    alert("Please enter your name");
+  } else if (x.length <= 2) {
+    alert("Min lenght is 3")
+  } else if (!isNaN(x)) {
+    alert("Numbers are not allowed");
+  } else if (y == "") {
+    alert("Please enter your crush's name");
+  } else if (y.length <= 2) {
+    alert("Min lenght is 3")
+  } else if (!isNaN(y)) {
+    alert("Numbers are not allowed");
+  } else {
 
-  
-  var text = "Hi " + x +"! The love percentage between you and "+ y + " is";
-  document.getElementById("demo").innerHTML = text;
+    var randomNumber = Math.floor((Math.random() * 100) + 1);
+
+    var text = "Hi " + x + "! The percentage of love between you and " + y + " is " + randomNumber + "%";
+    document.getElementById("demo").innerHTML = text;
+
+  }
 }
