ff01541cfda41d5203d0f0fe14f400bc966e0c39
mahimasingh25 <mahimasingh2509@gmail.com>
2020-08-26 21:57:22 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 function checklove() {
   var x = document.getElementById("yourname").value;
-  x = (x.slice(0,1)).toUpperCase() + (x.slice(1,x.length)).toLowerCase();
+  x = (x.slice(0, 1)).toUpperCase() + (x.slice(1, x.length)).toLowerCase();
 
   var y = document.getElementById("crushname").value;
-  y = (y.slice(0,1)).toUpperCase() + (y.slice(1,x.length)).toLowerCase();
+  y = (y.slice(0, 1)).toUpperCase() + (y.slice(1, x.length)).toLowerCase();
 
   if (x == "") {
     alert("Please enter your name");
@@ -23,8 +23,23 @@ function checklove() {
 
     var text = "Hi " + x + "! The percentage of love between you and " + y + " is: ";
 
+    var message = "";
+
+    if (randomNumber == 100){
+      message = "Are you guys even real?"
+    }else if (randomNumber >  80) {
+      message = "You hit the Jackpot.";
+    } else if (randomNumber >  40) {
+      message = "You can work on it.";
+    } else if (randomNumber > 20) {
+      message = "Time to find a new crush.";
+    } else {
+      message = "Don't be sad.";
+    }
+
     document.getElementById("message1").innerHTML = text;
     document.getElementById("percent").innerHTML = randomNumber + "%";
+    document.getElementById("message2").innerHTML = message;
 
   }
 }
